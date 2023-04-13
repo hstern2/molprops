8f65aeb50c016c3fc768569f9fe5e29504e8fc76
Ubuntu <ubuntu@ip-172-31-39-134.us-east-2.compute.internal>
2023-04-13 23:53:36 +0000

    changes

--- app.py
+++ app.py
@@ -28,4 +28,4 @@ def home():
     return h + (MolProps(request.form.get('smiles')) if isPost else '') + f
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=80)
